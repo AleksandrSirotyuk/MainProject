5b68af372f6b370b8cc8ef060c5dbd10edf53c93
tedBilgar <mcquicklyss@gmail.com>
2018-03-11 17:48:34 +0500

    adding name

--- Assets/MainMenu/GamerManager.cs
+++ Assets/MainMenu/GamerManager.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GamerManager : MonoBehaviour {
 
+    public Text usernameText;
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
+	void Start()
+    {
+        usernameText.text = GlobalControl.Instance.email;
+    }
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        usernameText.text = GlobalControl.Instance.email;
+    }
 }
